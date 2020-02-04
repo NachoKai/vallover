377b28a74bdc50744f50d5121b0a9c2906a88baa
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2020-02-04 11:39:59 -0300

    .

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ $(document).ready(() => {
         } else {
             alert('Disculpa, tu navegador no soporta geolocalización.');
         }
+
+        navigator.geolocation.getCurrentPosition(getLocationWeather, () =>
+            alert("no funciona si bloqueás la localización")
+        )
     }
 
     function getLocationWeather(position) {
